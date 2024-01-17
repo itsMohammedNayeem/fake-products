7beceb1daeb7bcfb01989b33cfce7c32d4f250e3
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2024-01-17 14:34:22 +0530

    Remove commented out code for fetching product details and user data

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -2,25 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ProductDetail = ({ product }) => {
-  // const [productDetail, setProductDetail] = useState({});
-
-  // useEffect(() => {
-  //   fetchProductDetails(product.productId).then((data) =>
-  //     setProductDetail(data)
-  //   );
-  // }, [product]);
-
-  // const fetchProductDetail = async () => {
-  //   For each productId, fetch details from the second API "https://fakestoreapi.com/products/2"
-
-  //   fetch(`https://fakestoreapi.com/products/${product.productId}`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setProductDetail(data);
-  //     })
-  //     .catch((error) => console.log(error));
-  // };
-
   return (
     <div className="p-8">
       <h2 className="text-lg">Product Detail</h2>
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,12 +3,7 @@ import { fetchCartProducts, fetchProductDetails } from "./apiService";
 import ProductDetail from "./ProductDetail";
 
 const ProductList = () => {
-  // State to store product data
   const [userData, setUserData] = useState([]);
-
-  //   useEffect(() => {
-  //     fetchCartProducts().then((data) => setUserData(data.products));
-  //   }, []);
 
   useEffect(() => {
     const fetchProductList = async () => {
@@ -28,17 +23,6 @@ const ProductList = () => {
     };
     fetchProductList();
   }, []);
-
-  //   const fetchProductList = async () => {
-  //     Fetch data from the first API "https://fakestoreapi.com/carts/2"
-
-  //     fetch("https://fakestoreapi.com/carts/2")
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         setUserData(data.products);
-  //       })
-  //       .catch((error) => console.log(error));
-  //   };
 
   return (
     <div className="p-8">
